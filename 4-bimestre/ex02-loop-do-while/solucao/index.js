b73f48db03e3723785f56d90bc4cb7b61f28a316
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2025-10-14 13:51:36 +0000

    solução resolvida

---
+++ 4-bimestre/ex02-loop-do-while/solucao/index.js
@@ -0,0 +1,12 @@
+// CRIE UMA LÓGICA AQUI
+let numero = 5
+let resultado = 0
+
+do {
+    resultado = numero
+    numero = numero + 1
+    console.log(`Contagem: ${resultado}`)
+} while (numero <= 10)
+
+// === NÃO MODIFIQUE NADA ABAIXO ============
+module.exports = resultado
