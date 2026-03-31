521f64c91428bb5c417df81e6fe44403047ae61d
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2026-03-31 08:01:17 -0300

    Add files via upload

--- 1-bimestre/programacao-web/projetos/projeto4-sistema-de-lista-de-tarefas/src/js/script.js
+++ 1-bimestre/programacao-web/projetos/projeto4-sistema-de-lista-de-tarefas/src/js/script.js
@@ -2,10 +2,33 @@ const { createElement } = require("react")
 
 const tarefa = document.getElementById("tarefa")
 const button = document.getElementById("adicionar")
+const lista = document.getElementById("lista")
 
-button.addEventListener("click", () => {
-    const item = createElement("li")
-    const remover = createElement("button")
+button.addEventListener("click", (event) => {
+    event.preventDefault()
 
-    
+    const item = document.createElement("li")
+
+    if (item === "") {
+        alert("digite uma tarefa válida")
+    }
+
+    item.textContent = tarefa
+
+    item.addEventListener("click", () => {
+
+        item.classList.add("concluido")
+})
+
+    const remover = document.createElement("button")
+    remover.textContent = "Remover"
+
+remover.addEventListener("click", () => {
+    item.remove()
+})
+
+item.appendChild(remover)
+lista.appendChild(item)
+
+input.value = ""
 })
