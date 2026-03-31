eaf3f0584ac727046a0f2fc56443bc3c67d59eba
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2026-03-31 11:06:50 +0000

    feito

--- 1-bimestre/programacao-web/ex13-manipulando-dom/src/js/script.js
+++ 1-bimestre/programacao-web/ex13-manipulando-dom/src/js/script.js
@@ -2,5 +2,5 @@ const mensagem = document.getElementById("mensagem")
 const botao = document.getElementById("botao")
 
 addEventListener("click", function alterar() {
-    mensagem.innerHTML = "Mensagem alterada com JavaScript"
+    mensagem.textContent = "Mensagem alterada com JavaScript"
 })
