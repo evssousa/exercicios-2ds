3f356ac98e8cb0fd55052d4c44b60a103020c6d1
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2026-04-27 22:02:34 +0000

    feito

---
+++ 2-bimestre/programacao-web/aula03-criando-um-projeto-com-vite/react/src/App.jsx
@@ -0,0 +1,5 @@
+export default function App() {
+  return (
+    <h1>Meu Primeiro Projeto React com Vite</h1>
+  )
+}
---
+++ 2-bimestre/programacao-web/aula04-criando-primeiros-componentes/react/src/App.jsx
@@ -0,0 +1,11 @@
+import {Titulo} from "./components/Titulo"
+import {Paragrafo} from "./components/Paragrafo"
+
+export default function App() {
+  return (
+    <div>
+      <Titulo />
+      <Paragrafo />
+    </div>
+  )
+}
