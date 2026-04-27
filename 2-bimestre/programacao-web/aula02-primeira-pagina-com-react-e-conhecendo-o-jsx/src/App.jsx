2a495673c1e5714a192f03f57caa4ce448ca37ab
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2026-04-27 21:24:08 +0000

    corrigido

--- 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
+++ 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
@@ -1,9 +1,10 @@
 export default function App() {
   const filmeFavorito = "101 Dalmatas"
+  const materiaFavorita = "Matemática"
   return (
     <div>
       <h1>Keirryson Raffael</h1>
-      <p>Matemática</p>
+      <p>{materiaFavorita}</p>
       <p>Meu filme favorito é: {filmeFavorito}</p>
     </div>
   )
