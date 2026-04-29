578e8666384edfc165d8d6ee3b0f86311f7f5f91
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2026-04-29 17:15:32 +0000

    feito

--- 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/react/src/pages/Contato.jsx
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/react/src/pages/Contato.jsx
@@ -2,7 +2,7 @@ export default function Contato() {
     return (
         <div>
             <h2>Contato</h2>
-            <p>contato@site.com ou 85992131233</p>
+            <p>contato@site.com ou (85)992131233</p>
         </div>
     )
 }
