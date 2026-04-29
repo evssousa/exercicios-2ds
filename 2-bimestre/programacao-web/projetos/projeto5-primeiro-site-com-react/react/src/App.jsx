63bb26bb9656fa0b00bacb734fb7b5b422222294
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2026-04-29 17:33:56 +0000

    feito

--- 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/react/src/App.jsx
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/react/src/App.jsx
@@ -1,8 +1,8 @@
-import {Header} from "./components/Header"
-import {Home} from "./pages/Home"
-import {Sobre} from "./pages/Sobre"
-import {Contato} from "./pages/Contato"
-import {Footer} from "./components/Footer"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+import Contato from "./pages/Contato"
+import Footer from "./components/Footer"
 
 export default function App() {
     return (
