bf74b0fec2ed9ac83679a4ab3c6adfcb5251acca
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2026-05-25 14:12:13 +0000

    terminando

--- 2-bimestre/programacao-web/projetos/projeto6-componente-profile/nivel2/react/src/App.jsx
+++ 2-bimestre/programacao-web/projetos/projeto6-componente-profile/nivel2/react/src/App.jsx
@@ -10,29 +10,36 @@ import Footer from "./components/Footer"
 export default function App(){
   return(
     <>
+      
       <div className="container">
         <Navbar/>
-        <div className="profile-list">
-          <Profile
-          nome={"Henrique"}
-          profissao={"Vendedor de gás"}
-          imagem={VendedorDegas}
-          idade={40}
-          />
+        <div className="principal">
+          <div className="tituloInfos">
+            <h1 className="titulo">Usuários do sistema</h1>
+            <h2 className="subTitulo">Conheça os membros da nossa equipe</h2>
+          </div>
+          <div className="profile-list">
+            <Profile
+            nome={"Henrique"}
+            profissao={"Vendedor de gás"}
+            imagem={VendedorDegas}
+            idade={40}
+            />
 
-          <Profile
-          nome={"Francisco"}
-          profissao={"Gari"}
-          imagem={Gari}
-          idade={35}
-          />
+            <Profile
+            nome={"Francisco"}
+            profissao={"Gari"}
+            imagem={Gari}
+            idade={35}
+            />
 
-          <Profile
-          nome={"Cordeiro"}
-          profissao={"garoto de programa"}
-          imagem={programador}
-          idade={26}
-          />
+            <Profile
+            nome={"Cordeiro"}
+            profissao={"garoto de programa"}
+            imagem={programador}
+            idade={26}
+            />
+          </div>
         </div>    
         <Footer/>
       </div>
